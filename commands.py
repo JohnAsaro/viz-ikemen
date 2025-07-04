4fc6cdd0c94835c7af0039fd4369f8e1d1023c27
JohnAsaro <johnnyasaro63@gmail.com>
2025-07-04 19:47:05 -0400

    moved taunt to action 45 so its the easiest to disable

--- commands.py
+++ commands.py
@@ -5,7 +5,7 @@ ACTIONS = ["None", "Triple Kung Fu Palm", "Smash Kung Fu Upper", "Kung Fu Blocki
             "Light KF Blow", "Strong KF Blow", "Fast KF Blow", "Light KF Zankou", "Strong KF Zankou", "Fast KF Zankou", 
             "Light KF Knee", "Strong KF Knee", "Fast KF Knee", "Forward Run", "Backdash", "Neutral Recovery", "Forward Recovery", 
             "Backward Recovery", "Upward Recovery", "Downward Recovery", "Light Punch", "Strong Punch", "Light Kick", "Strong Kick",
-            "Crouching Light Punch", "Crouching Strong Punch", "Crouching Light Kick", "Crouching Strong Kick", "Taunt",
+            "Crouching Light Punch", "Crouching Strong Punch", "Crouching Light Kick", "Crouching Strong Kick",
             "Forward Throw", "Back Throw", "Walk Forward", "Walk Back", "Jump", "Crouch", "Jump Forward", "Jump Back",
-            "Down Forward", "Down Back"]  # KFMs commands, index corresponds directly to the action mapping in external.zss
+            "Down Forward", "Down Back", "Taunt"]  # KFMs commands, index corresponds directly to the action mapping in external.zss
 
