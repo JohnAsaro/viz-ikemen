67a85d95d064d43baed7d10dc0c9a32f5346055e
JohnAsaro <johnnyasaro63@gmail.com>
2025-07-05 19:48:35 -0400

    added PPO_8 data

--- ikemen_wrapper_sb3.py
+++ ikemen_wrapper_sb3.py
@@ -591,9 +591,9 @@ def test_ppo(env, model_path, n_episodes=10):
 
 if __name__ == "__main__":
     n_steps = 8192 # Number of steps to take before revaluting the policy
-    env = IkemenEnv(ai_level=1, screen_width=80, screen_height=60, show_capture=True, n_steps=n_steps, showcase=False, step_delay = 0.00952380952, headless = True)  # Create the Ikemen environment
+    env = IkemenEnv(ai_level=1, screen_width=80, screen_height=60, show_capture=False, n_steps=n_steps, showcase=False, step_delay = 0.00952380952, headless = False)  # Create the Ikemen environment
     # Note: Screen width and height below 160x120 are wonkey on windows
     # env_checker.check_env(env)  # Check the environment
-    train_PPO(env, timesteps=3000000, check=250000, num_steps=n_steps)  # Train the PPO model
-    # test_ppo(env, model_path=os.path.join(RL_SAVES, "models", "PPO_7", "best_model_1250000.zip"), n_episodes=10)  # Test the trained model
+    # train_PPO(env, timesteps=3000000, check=250000, num_steps=n_steps)  # Train the PPO model
+    test_ppo(env, model_path=os.path.join(RL_SAVES, "models", "PPO_8", "best_model_3000000.zip"), n_episodes=10)  # Test the trained model
 
