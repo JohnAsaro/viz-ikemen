96181bc5ebc5ee87f33c1b2ad576f8c990857429
JohnAsaro <johnnyasaro63@gmail.com>
2025-07-07 17:21:49 -0400

    just gonna push the episode logs for now because the stupid computer im using isnt letting me use juypter notebook for some reason and that would make graphing such a huge pain

--- ikemen_wrapper_sb3.py
+++ ikemen_wrapper_sb3.py
@@ -648,6 +648,6 @@ if __name__ == "__main__":
     env = IkemenEnv(ai_level=1, screen_width=80, screen_height=60, show_capture=True, n_steps=n_steps, showcase=False, step_delay = 0.00119047619, headless = True, speed = 126, fps = 840)  # Create the Ikemen environment
     # Note: Screen width and height below 160x120 are wonkey on windows
     # env_checker.check_env(env)  # Check the environment
-    train_PPO(env, timesteps=3000000, check=250000, num_steps=n_steps)  # Train the PPO model
+    train_PPO(env, timesteps=8000000, check=250000, num_steps=n_steps)  # Train the PPO model
     # test_ppo(env, model_path=os.path.join(RL_SAVES, "models", "PPO_8", "best_model_3000000.zip"), n_episodes=10)  # Test the trained model
 
